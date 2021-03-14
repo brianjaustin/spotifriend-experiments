6909aa88c1a29797f504c2e01556b4ecd96ad4df
Brian Austin <brianjaustin@gmail.com>
2021-03-14 17:15:23 -0400

    Remove bad API

--- experiment/mix.exs
+++ experiment/mix.exs
@@ -41,9 +41,7 @@ defmodule Experiment.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:tesla, "~> 1.2"},
-      {:poison, "~> 3.0"}
+      {:plug_cowboy, "~> 2.0"}
     ]
   end
 
