1815425e3708a30fab78b944d57dcca78f4be1e4
Liam Brown <liamnb525@gmail.com>
2021-03-14 17:08:40 -0400

    Placeholder stuff that doesnt really do anything.

--- spotify-oauth/spotify-oauth/src/App.js
+++ spotify-oauth/spotify-oauth/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { Login } from './login/Login';
+import { useState } from 'react';
 
 function App() {
 
+  let loginInfo = useState({loggedIn: false});
+
   return (
     <div className="App">
-      <header className="App-header">
-        Header
-      </header>
-      <Login></Login>
+      <header className="App-header">Header</header>
+      {!loginInfo.loggedIn && <Login></Login>}
     </div>
   );
 }
---
+++ spotify-oauth/spotify-oauth/src/login/Register.js
@@ -0,0 +1,9 @@
+import "./Register.css";
+
+export function Login() {
+  return (
+    <div className="Register">
+      <h1>Register</h1>
+    </div>
+  );
+}
