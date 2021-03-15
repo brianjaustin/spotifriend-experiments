ea48aab827db0873a7d18cd92f143a9c8d871d05
Brian Austin <brianjaustin@gmail.com>
2021-03-14 22:00:08 -0400

    Test code for auth token flow

--- melody_match/lib/melody_match_web/controllers/page_controller.ex
+++ melody_match/lib/melody_match_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule MelodyMatchWeb.PageController do
   use MelodyMatchWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    client_id = Application.get_env(:melody_match, :spotify)[:client_id]
+    render(conn, "index.html", client_id: client_id)
   end
 end
--- melody_match/lib/spotify.ex
+++ melody_match/lib/spotify.ex
@@ -6,6 +6,44 @@ defmodule Spotify do
   0 retries.
   """
 
+  alias MelodyMatch.Accounts
+
+  def get_tokens(user_id, :code, code) do
+    url = "https://accounts.spotify.com/api/token"
+    body = {:form, [
+      {"grant_type", "authorization_code"},
+      {"code", code},
+      # TODO: this should be in config too
+      {"redirect_uri", "http://localhost:4000/spotify/callback"}
+    ]}
+    
+    client_id = Application.get_env(:melody_match, :spotify)[:client_id]
+    client_secret = Application.get_env(:melody_match, :spotify)[:client_secret]
+    auth = Base.encode64("#{client_id}:#{client_secret}")
+    headers = ["Authorization": "Basic #{auth}", "Content-Type": "application/x-www-form-urlencoded"]
+
+    response = HTTPoison.post!(url, body, headers)
+    if response.status_code == 200 do
+      toks = Jason.decode!(response.body)
+
+      IO.inspect toks
+      
+      # TODO: make this be an upsert and remove the update function
+      Accounts.create_spotify_token(%{
+        user_id: user_id,
+        auth_token: toks["access_token"],
+        refresh_token: toks["refresh_token"]
+      })
+    else
+      {:error, response.body}
+    end
+  end
+
+  def get_tokens(user_id, :refresh, tokens) do
+    # TODO
+    %{}
+  end
+
   def get_top_songs(tokens, limit \\ 1, retries \\ 1) do
     url = "https://api.spotify.com/v1/me/top/tracks?limit=#{limit}"
     headers = ["Authorization": "Bearer #{tokens.auth_token}"]
